65a9d0803eee59b4fea02d53baf58dabf96a8e43
Abdurohman <su1600439@su.edu.et>
2026-07-30 23:42:54 +0300

    Add Category model , display badges on product list and Improve UI styling and category filter buttons

--- store/models.py
+++ store/models.py
@@ -1,5 +1,14 @@
 from django.db import models
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
+    def __str__(self):
+        return self.name
 class Product(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True ,blank=True)
     name = models.CharField(max_length=100)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
--- store/views.py
+++ store/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render , get_object_or_404
-from .models import Product
+from .models import Product , Category
 def product_list(request):
-    products = Product.objects.all()
-    return render (request, 'store/product_list.html' , {'products': products})
+    category_id = request.GET.get('category')
+    categories = Category.objects.all()
+    if category_id:
+        products = Product.objects.filter(category_id=category_id)
+    else:
+        products = Product.objects.all()
+    return render (request, 'store/product_list.html' , {'products': products, 'categories': categories})
 def product_detail(request,pk):
     product = get_object_or_404(Product,pk=pk)
     return render(request,'store/product_detail.html',{'product':product})

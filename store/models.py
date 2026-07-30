4f3136e14180ff28f95d631296777e9cdb4fc22c
Abdurohman <su1600439@su.edu.et>
2026-07-30 07:04:19 +0300

    Add product images and update detail page template

--- store/models.py
+++ store/models.py
@@ -4,6 +4,7 @@ class Product(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.PositiveIntegerField()
+    image = models.ImageField(upload_to='products/', null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
--- store/urls.py
+++ store/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns=[
     path('',views.product_list , name="product_list"),
     path('product/<int:pk>/', views.product_detail, name='product_detail'),
+
 ]
+if settings.DEBUG:
+    from django.conf import settings
+    from django.conf.urls.static import static
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

a37a83d6c78de5bf8d43dfb8193fccb1efb18730
Abdurohman <su1600439@su.edu.et>
2026-07-31 08:47:29 +0300

    Add product search,product count, and user signup system

--- store/urls.py
+++ store/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns=[
     path('',views.product_list , name="product_list"),
     path('product/<int:pk>/', views.product_detail, name='product_detail'),
-
+    path('signup/',views.views_signup if hasattr(views,'views_signup') else views.signup, name='signup'),
+    path('login/' , auth_views.LoginView.as_view(template_name = 'store/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page= 'producy_list'),name='logout'),
 ]
 if settings.DEBUG:
     from django.conf import settings
--- store/views.py
+++ store/views.py
@@ -1,13 +1,40 @@
-from django.shortcuts import render , get_object_or_404
+from django.shortcuts import render , get_object_or_404 , redirect
 from .models import Product , Category
+from django.db.models import Q
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 def product_list(request):
     category_id = request.GET.get('category')
+    search_query = request.GET.get('search')
     categories = Category.objects.all()
+    products = Product.objects.all()
     if category_id:
         products = Product.objects.filter(category_id=category_id)
+        selected_category = int(category_id)
     else:
-        products = Product.objects.all()
-    return render (request, 'store/product_list.html' , {'products': products, 'categories': categories})
+        selected_category = None
+    if search_query:
+        products = products.filter(Q(name__icontains=search_query) | Q(description__icontains=search_query))
+    product_count = products.count()
+
+    
+    return render (request, 'store/product_list.html' , {
+        'products': products,
+        'categories': categories, 
+        'product_count': product_count, 
+        'selected_category': selected_category,
+        'search_query': search_query
+    })
 def product_detail(request,pk):
     product = get_object_or_404(Product,pk=pk)
     return render(request,'store/product_detail.html',{'product':product})
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'አካውንትህ  በትክክል ተፈጥሯል! እባክዎን ወደ መግቢያ ገጽ ይግቡ.')
+            return redirect('login')  # Redirect to the login page after successful signup
+    else:
+        form = UserCreationForm()
+    return render(request, 'store/signup.html', {'form': form})

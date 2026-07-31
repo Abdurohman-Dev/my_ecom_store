6c45defb872eb865361827ff1624214aa9983fa0
Abdurohman <su1600439@su.edu.et>
2026-07-31 22:44:59 +0300

    Complete login,logout and authentication setup with URL redirects

--- store/urls.py
+++ store/urls.py
@@ -8,7 +8,7 @@ urlpatterns=[
     path('product/<int:pk>/', views.product_detail, name='product_detail'),
     path('signup/',views.views_signup if hasattr(views,'views_signup') else views.signup, name='signup'),
     path('login/' , auth_views.LoginView.as_view(template_name = 'store/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(next_page= 'producy_list'),name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(next_page= 'product_list'),name='logout'),
 ]
 if settings.DEBUG:
     from django.conf import settings
